365d8855bad078901f0286fb9c06654bb516bac1
thuns <thuns@debian.co>
2024-06-18 12:38:14 -0600

    Barebones frontend setup, going to build out the backend a little and then come back to this

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import PartSelector from "$lib/components/PartSelector.svelte";
+</script>
+<main>
+    <h1>Create a satellite</h1>
+    <p>Select Parts</p>
+    <PartSelector />
+    <p>Build satellite</p>
+</main>
